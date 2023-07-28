1c93bec8a49fa72dcccc94aa3a5de20ef6cffbbc
AquitaB <139923723+AquitaB@users.noreply.github.com>
2023-07-28 13:25:34 -0500

    Cleaned Version of the Intranet
    New Vector Images Added

---
+++ Index.js
@@ -0,0 +1,31 @@
+﻿//function updateProgressBar() {
+//    if (document.getElementById('form').checked) {
+//        console.log(alert("Checked"));
+//    } else {
+//        alert("Not Checked!")
+//    }
+//}
+
+//updateProgressBar();
+
+//document.querySelector("button").addEventListener("click", function () {
+//    let checked = document.querySelectorAll("input[type='checkbox']:checked");
+//    console.log('IT WORKS');
+//    checked / forEach(function (cb) {
+//        console.log(cb.value);
+//    });
+//})
+
+//form = document.getElementById('form');
+//formChecked = form.setAttribute("checked", "checked");
+//if (form == formChecked) {
+//    console.log("Test!")
+//}
+
+const formQuery = document.querySelector("input");
+
+formQuery.setAttribute("checked", "checked");
+form = document.getElementById('form');
+if (form == formChecked) {
+    console.log("Test!")
+}
